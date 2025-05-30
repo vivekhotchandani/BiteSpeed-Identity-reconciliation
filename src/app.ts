4584ebea317a3de23f415af53e5836370c57c61e
vivekhotchandani <vivekhotchandani2514@gmail.com>
2025-05-30 23:42:19 +0530

    added /identity endpoint in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express, { RequestHandler } from 'express';
+import identifyRouter from './routes/identify';
 
 const app = express();
 app.use(express.json() as RequestHandler);
+app.use('/', identifyRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
